5139339e897dbb26398d647c69c8f2aeabfb4cce
Zaid Khan <zaid89779.com>
2022-01-26 16:01:49 +0500

    complete category component

--- components/Categories.js
+++ components/Categories.js
@@ -40,6 +40,14 @@ const items = [
 
 export default function Categories() {
   return (
+    <View style={{
+      marginTop:5,
+      backgroundColor
+      :"#fff",
+      paddingVertical:10,
+      paddingLeft:20,
+
+    }}>
     <ScrollView horizontal showsHorizontalScrollIndicator={false}>
       {items.map((item,index)=>(<View style={{ alignItems: "center", marginRight: 30 }}>
         <Image source={item.image}
@@ -52,6 +60,6 @@ export default function Categories() {
         <Text style={{ fontSize: 13, fontWeight: "900" }}>{item.text}</Text>
       </View>))}
       
-    </ScrollView>
+    </ScrollView></View>
   );
 }
